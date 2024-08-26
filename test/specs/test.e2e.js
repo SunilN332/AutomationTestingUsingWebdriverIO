fe8445f9fc9ccddcc2a9fabe7a7f621eeb2e7171
sunilNreddy332 <reddysunil336@gmail.com>
2024-08-25 20:14:41 -0700

    upated  shipment and test.e2e.js files

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,4 +1,4 @@
-import { expect } from '@wdio/globals';
+
 import LoginPage from '../pageobjects/login.page.js';
 import SecurePage from '../pageobjects/secure.page.js';
 import config from '../Lib/config-qaint.js';
@@ -6,7 +6,7 @@ import { BASE_URLS } from '../utils/urls.js';
 
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
-       await loginPage.open(BASE_URLS.swagLabsUrl);
+       await LoginPage.open(BASE_URLS.swagLabsUrl);
         await LoginPage.login(config.userName, config.password);
     });
 });
